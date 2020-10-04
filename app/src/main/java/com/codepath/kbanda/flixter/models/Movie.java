c4bc0abb5e1dd4bb2fa879901cb1be19c1c46441
Kinnera Banda <kinnerabanda@gmail.com>
2020-10-03 20:07:21 -0400

    Youtube trailer minimum integration

--- app/src/main/java/com/codepath/kbanda/flixter/models/Movie.java
+++ app/src/main/java/com/codepath/kbanda/flixter/models/Movie.java
@@ -13,6 +13,7 @@ import java.util.List;
 @Parcel
 public class Movie {
 
+    int movieId;
     String posterPath;
     String title;
     String overview;
@@ -28,6 +29,7 @@ public class Movie {
         overview = jsonObject.getString("overview");
         backdropPath = jsonObject.getString("backdrop_path");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException {
@@ -57,5 +59,9 @@ public class Movie {
 
     public double getRating() {
         return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
